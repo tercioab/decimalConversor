5c021963ba4372faf810f8a488ba71ee15e1df34
tercioab <suaagenciainicial@gmail.com>
2023-07-13 14:10:17 -0300

    test: test component and functions

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function Convert() {
+export default function Page() {
 	const [numberAndType, setNumberAndType] = useState({ number: 0, type: "Binário" });
 	const [lista, setLista] = useState(false);
 	const [result, setResult] = useState([]);
@@ -83,13 +83,13 @@ export default function Convert() {
 	}
 
   return (
-    <div class="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen">
       
-      <div class="max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow dark:bg-slate-800 dark:border-slate-700">
+      <div className="max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow dark:bg-slate-800 dark:border-slate-700">
       <p className="text-center text-2xl font-extrabold mt-10 text-transparent bg-clip-text bg-gradient-to-r to-sky-400 from-indigo-500">CONVERSÕES DE BASE DECIMAL</p>
 
 		<form onSubmit={onSubmit} className='flex flex-col items-center mt-8'>
-			<label for='number' class='block  text-sm font-medium text-slate-900 '>
+			<label htmlFor='number' className='block  text-sm font-medium text-slate-900 '>
 				Insira um valor numérico:
 			</label>
 			<input
@@ -102,7 +102,7 @@ export default function Convert() {
 				placeholder='Insira um valor numérico'
 			/>
 
-			<label for='type' class='block  text-sm font-medium text-slate-900 '>
+			<label htmlFor='type' className='block  text-sm font-medium text-slate-900 '>
 				Selecione o tipo de conversão:
 			</label>
 			<select
@@ -116,7 +116,7 @@ export default function Convert() {
 				<option value='Octal'>Octal</option>
 			</select>
 
-			<label for='lista' class='block  text-sm font-medium text-slate-900 '>
+			<label htmlFor='lista' className='block  text-sm font-medium text-slate-900 '>
 				Tipo de exibição:
 			</label>
 			<select
@@ -130,7 +130,7 @@ export default function Convert() {
 
 			<button
 				type='submit'
-				class='text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center '
+				className='text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center '
 			>
 				Enviar
 			</button>
